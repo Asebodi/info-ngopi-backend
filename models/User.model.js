fdc024619a42ae9fc063b97cc3bdf2aa5cc40bbc
Asebodi <aditraharditya45@gmail.com>
2020-09-13 17:40:33 +0700

    Added user model

---
+++ models/User.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const User = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", User);
+
+// tambah use ObjectId, rating, posts, dll
